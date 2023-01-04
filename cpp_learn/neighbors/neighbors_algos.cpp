b6c9dd5e14f9dbacf16db83e015e54c106e1d927
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-04 19:08:40 +0000

    initialised KNN implementation

---
+++ cpp_learn/neighbors/neighbors_algos.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include "../../numcpp/np_define.h"
+
+std::vector<int> predict(std::vector<std::vector<float>> x, std::vector<std::vector<int>> y){
+    std::vector<int> res;
+    return res;
+}
