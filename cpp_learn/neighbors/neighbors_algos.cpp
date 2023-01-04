6f736c7984e5869da0e40ced393c616c50b0189e
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-04 19:21:11 +0000

    refactored KNN for class

--- cpp_learn/neighbors/neighbors_algos.cpp
+++ cpp_learn/neighbors/neighbors_algos.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <vector>
 #include "../../numcpp/np_define.h"
+#include <map>
 
-std::vector<int> predict(std::vector<std::vector<float>> x, std::vector<std::vector<int>> y){
-    std::vector<int> res;
-    return res;
+class KNNPredict{
+    public:
+        std::vector<std::vector<int>> y;
+        std::vector<std::vector<float>> x;
+
+        std::vector<std::vector<int>> predict(std::vector<std::vector<float>> x_pred){
+            std::vector<std::vector<int>> pred;
+            return pred;
+        }
 }
