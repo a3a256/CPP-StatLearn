b18243ad7ee6dda2427c580359c5a1c6a13c3486
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-08 19:52:08 +0000

    started mean_squared error

--- cpp_learn/metrics/evaluation.cpp
+++ cpp_learn/metrics/evaluation.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <vector>
 #include "../../numcpp/np_define.h"
 #ifndef mean_calculate();
 #define mean_calculate();
 #endif
+
+float mean_squared_error(std::vector<std::vector<float>> y_pred, std::vector<std::vector<float>> y_test){
+    return 0.0f;
+}
