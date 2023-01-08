4521e30fa3c5604227c10ef02dfc0944d2da2898
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-08 20:00:11 +0000

    implemented MSE

--- cpp_learn/metrics/evaluation.cpp
+++ cpp_learn/metrics/evaluation.cpp
@@ -6,5 +6,9 @@
 #endif
 
 float mean_squared_error(std::vector<std::vector<float>> y_pred, std::vector<std::vector<float>> y_test){
-    return 0.0f;
+    float _sum = 0.0f;
+    for(int i = 0; i<y_pred.size(); i++){
+        _sum += pow(abs(y_pred[i][0] - y_test[i][0]), 2);
+    }
+    return _sum/(float)y_pred.size();
 }
