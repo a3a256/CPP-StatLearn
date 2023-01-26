abb7ad9ae033c21cc87c2e5405137399368b69cb
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-26 23:37:08 +0000

    Initialized Linear Discriminant Analysis

---
+++ cpp_learn/discriminant_analysis/discriminant_analaysis_algorithms.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include "../../numcpp/np_define.h"
+
+class LinearDiscriminantAnalysis{
+    private:
+        std::vector<std::vector<std::vector<float>>> cov_matrices;
+};
