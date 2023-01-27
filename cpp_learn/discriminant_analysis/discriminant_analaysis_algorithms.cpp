a3c3b7bb99c7ed5ae25c562866ce6734e3606a4c
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-27 23:55:04 +0000

    fit for cov matrices

--- cpp_learn/discriminant_analysis/discriminant_analaysis_algorithms.cpp
+++ cpp_learn/discriminant_analysis/discriminant_analaysis_algorithms.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include "../../numcpp/np_define.h"
 
-class LinearDiscriminantAnalysis{
-    private:
-        std::vector<std::vector<std::vector<float>>> cov_matrices;
-        std::vector<std::vector<float>> means;
-};
+
+std::vector<std::vector<std::vector<float>>> fit_cov_lda(std::vector<std::vector<float>> x, std::vector<std::vector<float>> y){
+    std::vector<int> categories;
+    categories = classes(y);
+}
