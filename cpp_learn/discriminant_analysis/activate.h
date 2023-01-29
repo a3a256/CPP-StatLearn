dc44277de847ae436e6d4832e7fcf628e0f2f491
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-29 23:06:59 +0000

    fit for LDA

--- cpp_learn/discriminant_analysis/activate.h
+++ cpp_learn/discriminant_analysis/activate.h
@@ -9,4 +9,12 @@ class LinearDiscriminantAnalysis{
         std::vector<std::vector<std::vector<float>>> covariances;
         std::vector<std::vector<float>> means;
         std::vector<float> priors;
+
+
+    public:
+        void fit(std::vector<std::vector<float>> x, std::vector<std::vector<int>> y){
+            covariances = fit_cov_lda(x, y);
+            means = fit_mean_lda(x, y);
+            priors = calculate_piors(y);
+        }
 };
