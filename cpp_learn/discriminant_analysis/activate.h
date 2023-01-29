ec645211e7de08e53db67bfa90387a3c7c853b7d
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-01-29 23:01:13 +0000

    Added priors, covs, means

--- cpp_learn/discriminant_analysis/activate.h
+++ cpp_learn/discriminant_analysis/activate.h
@@ -1,4 +1,12 @@
 #pragma once
 #include <iostream>
+#include "discriminant_analaysis_algorithms.cpp"
 #include <vector>
-#include "discriminant_analysis_algorithms.cpp"
+
+
+class LinearDiscriminantAnalysis{
+    private:
+        std::vector<std::vector<std::vector<float>>> covariances;
+        std::vector<std::vector<float>> means;
+        std::vector<float> priors;
+};
