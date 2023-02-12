0758f1831b47442bf80275f5da12c6379018f47d
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-02-12 23:52:20 +0000

    initialised svm using sgd optimisation

---
+++ cpp_learn/svm/svm_algos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include "../../numcpp/np_define.h"
+
+
+class SGDSVC{
+    private:
+        std::vector<std::vector<float>> weights;
+
+
+    public:
+
+        void fit(std::vector<std::vector<float>> x, std::vector<std::vector<float>> y){
+            return;
+        }
+};
