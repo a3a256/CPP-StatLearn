706dbda16b335c5552c83bd9b644c54f957465d4
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-02-13 23:51:59 +0000

    started fit function

--- cpp_learn/svm/svm_algos.cpp
+++ cpp_learn/svm/svm_algos.cpp
@@ -7,11 +7,18 @@
 class SGDSVC{
     private:
         std::vector<std::vector<float>> weights;
+        float lr;
 
 
     public:
 
         void fit(std::vector<std::vector<float>> x, std::vector<std::vector<float>> y){
-            return;
+            std::vector<float> temp;
+            int i, j;
+            for(i = 0; i<x[0].size(); i++){
+                temp.push_back(0.001f);
+            }
+            weights.push_back(temp);
+            std::vector<float>().swap(temp);
         }
 };
