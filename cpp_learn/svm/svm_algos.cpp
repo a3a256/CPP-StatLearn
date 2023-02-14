2d42e594a5848b0429d34aeac193b656de7cc4e6
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-02-14 00:11:30 +0000

    implemented fit function

--- cpp_learn/svm/svm_algos.cpp
+++ cpp_learn/svm/svm_algos.cpp
@@ -7,18 +7,44 @@
 class SGDSVC{
     private:
         std::vector<std::vector<float>> weights;
-        float lr;
 
 
     public:
 
-        void fit(std::vector<std::vector<float>> x, std::vector<std::vector<float>> y){
+        std::vector<std::vector<float>> fit(std::vector<std::vector<float>> x, std::vector<std::vector<int>> y, float lr, int epochs, float C){
             std::vector<float> temp;
+            std::vector<std::vector<float>> temp_d;
+            std::vector<std::vector<float>> grad;
             int i, j;
+            float error, grad_val;
+            float hat;
             for(i = 0; i<x[0].size(); i++){
                 temp.push_back(0.001f);
             }
             weights.push_back(temp);
             std::vector<float>().swap(temp);
+
+            for(i = 0; i<epochs; i++){
+                for(j = 0; j<x.size(); j++){
+                    temp_d.push_back(x[j]);
+                    hat = dot(temp_d, transpose(weights))[0][0];
+                    error = 1 - (float)y[j][0]*hat;
+                    if(error <= 0){
+                        grad = weights;
+                    }else{
+                        for(int k = 0; k<x[0].size(); k++){
+                            temp.push_back(weights[0][k] - C*(float)y[j][0]*x[j][k]);
+                        }
+                        grad.push_back(temp);
+                    }
+
+                    for(int k = 0; k<x[0].size(); k++){
+                        weights[0][k] -= lr*grad[0][k];
+                    }
+                }
+            }
+
+
+            return weights;
         }
 };
