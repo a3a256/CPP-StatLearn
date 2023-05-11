4fde8205b0dcc462968743fdaf623ce0098e663b
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-05-11 02:09:43 +0100

    added to svm

--- cpp_learn/svm/svm_algos.cpp
+++ cpp_learn/svm/svm_algos.cpp
@@ -55,6 +55,13 @@ class SGDSVC{
                     for(int k = 0; k<x[0].size(); k++){
                         weights[0][k] -= lr*grad[0][k];
                     }
+
+                    if(is_null(weights)){
+                        weights = backup;
+                        break;
+                    }else{
+                        backup = weights;
+                    }
                     std::vector<std::vector<float>>().swap(grad);
                     std::vector<std::vector<float>>().swap(temp_d);
                     std::vector<float>().swap(temp);
