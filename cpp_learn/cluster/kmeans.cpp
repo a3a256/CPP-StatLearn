40fb8855c0b2d11ed3cfd52c7c074a53a6eb11dc
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-05-22 00:36:36 +0100

    implemented first k points selection

--- cpp_learn/cluster/kmeans.cpp
+++ cpp_learn/cluster/kmeans.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include <cmath>
 
-std::vector<std::vector<float>> initialise(std::vector<std::vector<float>>, std::vector<std::vector<int>> y){
-    return;
+std::vector<std::vector<float>> initialise(std::vector<std::vector<float>> x, int k){
+    std::vector<std::vector<float>> weights;
+    int i;
+    for(i=0; i<k; i++){
+        weights.push_back(x[i]);
+    }
+    return weights;
 }
