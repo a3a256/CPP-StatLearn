3a4ad94ac44bdd033a1b427e2e15f9d1d74f5aaf
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-05-21 23:47:06 +0100

    initialised kmean

---
+++ cpp_learn/cluster/kmeans.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+std::vector<std::vector<float>> initialise(std::vector<std::vector<float>>, std::vector<std::vector<int>> y){
+    return;
+}
