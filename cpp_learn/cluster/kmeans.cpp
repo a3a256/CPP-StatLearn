7f7e98f927103d7932cf4fd8cb16e8a6b38b8536
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-05-26 01:52:00 +0100

    started kmean fit function

--- cpp_learn/cluster/kmeans.cpp
+++ cpp_learn/cluster/kmeans.cpp
@@ -10,3 +10,9 @@ std::vector<std::vector<float>> initialise(std::vector<std::vector<float>> x, in
     }
     return weights;
 }
+
+std::vector<std::vector<float>> kmeans_fit(std::vector<std::vector<float>> x, int k){
+    std::vector<std::vector<float>> weight;
+    weight = initialise(x, k);
+    return weight;
+}
