c3b1f69491621a8ae7a99cbc2983531c3fc74bd7
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-12-07 23:43:01 +0000

    added distance and slicing for each label

--- cpp_learn/cluster/kmeans.cpp
+++ cpp_learn/cluster/kmeans.cpp
@@ -11,6 +11,21 @@ std::vector<std::vector<float>> initialise(std::vector<std::vector<float>> x, in
     return weights;
 }
 
+float distance(float x, float y){
+    return std::abs(x-y);
+}
+
+std::vector<float> slice(int label, int feature, std::vector<std::vector<float>> &vals, std::vector<float> &classified){
+    std::vector<float> temp;
+    int i;
+    for(i=0; i<vals.size(); i++){
+        if(classified[i] == label){
+            temp.push_back(vals[i][feature]);
+        }
+    }
+    return temp;
+}
+
 std::vector<std::vector<float>> kmeans_fit(std::vector<std::vector<float>> x, int k){
     std::vector<std::vector<float>> weight;
     weight = initialise(x, k);
